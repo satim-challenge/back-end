79f2f8e2a599b7e937ecc5f451092b36c7954d9c
Nesrine BOUZID <124919116+nesrine-aibot@users.noreply.github.com>
2025-06-21 09:11:50 +0100

    Update TransactionController.js

--- src/controllers/TransactionController.js
+++ src/controllers/TransactionController.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const SLR = require('ml-regression').SLR;
+
+
 const Transaction = require('../models/Transaction');
 exports.process = async (req, res) => {
   const transaction = new Transaction(req.body);
@@ -7,3 +12,38 @@ exports.process = async (req, res) => {
 exports.predictNext = (req, res) => {
   res.send('Prediction logic here');
 };
+
+
+
+exports.forecastSpending =(req, res) => {
+  try {
+    const { userId } = req.params;
+    const modelPath = path.join(__dirname, `../models_cache/model_${userId}.json`);
+
+    if (!fs.existsSync(modelPath)) {
+      return res.status(404).json({ message: "Model not trained yet for this user." });
+    }
+
+    const { slope, intercept } = JSON.parse(fs.readFileSync(modelPath, 'utf8'));
+
+    const model = new SLR([0], [0]); // dummy to init
+    model.slope = slope;
+    model.intercept = intercept;
+
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 7);
+    const futureX = futureDate.getTime();
+
+    const predicted = model.predict(futureX);
+
+    res.json({
+      message: "Prediction success",
+      forecastDate: futureDate.toISOString(),
+      predictedAmount: predicted.toFixed(2)
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Prediction failed", error: error.message });
+  }
+};
+
